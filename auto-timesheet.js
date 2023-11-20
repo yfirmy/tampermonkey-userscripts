d3d4ec52519507ae4db504708a51d7804b6b4456
yfirmy <github@firmy.fr>
2023-11-20 13:01:28 +0100

    Update auto-timesheet.js
    Added absence checks

--- auto-timesheet.js
+++ auto-timesheet.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Auto Timesheet
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Automatic Timesheet Filler
 // @author       Yohan FIRMY (yfirmy)
 // @match        https://*/psc/fsprda/EMPLOYEE/ERP/c/NUI_FRAMEWORK.PT_AGSTARTPAGE_NUI.GBL*
@@ -14,12 +14,57 @@
 (
 function() {
     'use strict';
-    function fillField(doc, weekday, fieldId, fieldValue) {
-       let element = doc.getElementById(fieldId)
+    var presenceByDay;
+
+    function fillField(doc, weekday, fieldType, fieldId, fieldValueIfPresent, fieldValueIfAbsent) {
+       let element = doc.querySelector(fieldType + "[id='" + fieldId + "']");
        if(element) {
-          element.value = fieldValue;
+           console.log(presenceByDay);
+          if(presenceByDay && presenceByDay.get(weekday)) {
+             element.value = fieldValueIfPresent;
+          } else {
+             element.value = fieldValueIfAbsent;
+          }
           element.onchange();
-       } else console.log("Element " + fieldId + " was not found");
+       } else console.log("Element input[id='" + fieldId + "'] was not found");
+    }
+    function weekDayAsInt(weekday) {
+        let result;
+        switch(weekday) {
+            case 'Sunday':    result=1; break;
+            case 'Monday':    result=2; break;
+            case 'Tuesday':   result=3; break;
+            case 'Wednesday': result=4; break;
+            case 'Thursday':  result=5; break;
+            case 'Friday':    result=6; break;
+            case 'Saturday':  result=7; break;
+            default:          result=undefined;
+        }
+        return result;
+    }
+    function checkPresenceOnDay(doc, weekday) {
+        let result = true;
+        let day = weekDayAsInt(weekday);
+        let line = 0;
+        let absenceElementOnSameDay;
+        do {
+            absenceElementOnSameDay = doc.querySelector("input[id='POL_TIME"+day+"$"+line+"']");
+            if(absenceElementOnSameDay && absenceElementOnSameDay.value && absenceElementOnSameDay.value!='') {
+              result = false;
+              break;
+            }
+            line = line + 1;
+        } while(absenceElementOnSameDay);
+        return result;
+    }
+    function checkPresence(doc) {
+        let result = new Map();
+        result.set("Monday",    checkPresenceOnDay(doc, "Monday"));
+        result.set("Tuesday",   checkPresenceOnDay(doc, "Tuesday"));
+        result.set("Wednesday", checkPresenceOnDay(doc, "Wednesday"));
+        result.set("Thursday",  checkPresenceOnDay(doc, "Thursday"));
+        result.set("Friday",    checkPresenceOnDay(doc, "Friday"));
+        return result;
     }
     function checkTimesheet(changes, observer) {
 
@@ -28,61 +73,64 @@ function() {
 
        if(iframeTimesheet) {
           let innerDocument = iframeTimesheet.contentDocument ? iframeTimesheet.contentDocument : iframeTimesheet.contentWindow.document;
-          if(innerDocument.getElementById("TIME1$0")){
-              // Working hours per day
-              fillField(innerDocument, "Monday",    "TIME2$0", '7,70');
-              fillField(innerDocument, "Tuesday",   "TIME3$0", '7,70');
-              fillField(innerDocument, "Wednesday", "TIME4$0", '7,70');
-              fillField(innerDocument, "Thursday",  "TIME5$0", '7,70');
-              fillField(innerDocument, "Friday",    "TIME6$0", '7,70');
-              fillField(innerDocument, "Comments", "EX_TIME_HDR_COMMENTS", "(Automatically filled timesheet - see github.com/yfirmy/tampermonkey-userscripts)");
+          if(innerDocument.querySelector("input[id='TIME1$0']")){
+
+              presenceByDay = checkPresence(innerDocument);
+
+              // Working hours per day (in hours)
+              fillField(innerDocument, "Monday",    "input", "TIME2$0", '7,70', '');
+              fillField(innerDocument, "Tuesday",   "input", "TIME3$0", '7,70', '');
+              fillField(innerDocument, "Wednesday", "input", "TIME4$0", '7,70', '');
+              fillField(innerDocument, "Thursday",  "input", "TIME5$0", '7,70', '');
+              fillField(innerDocument, "Friday",    "input", "TIME6$0", '7,70', '');
+              fillField(innerDocument, "Comments",  "textarea", "", "EX_TIME_HDR_COMMENTS", "(Automatically filled timesheet - see github.com/yfirmy/tampermonkey-userscripts)");
           }
        }
        if(iframeAdditionalInfo) {
           let innerDocument = iframeAdditionalInfo.contentDocument ? iframeAdditionalInfo.contentDocument : iframeAdditionalInfo.contentWindow.document;
-          if(innerDocument.getElementById("UC_DAILYREST1$0")){
+          if(innerDocument.querySelector("select[id='UC_DAILYREST1$0']")){
 
-              // Daily rest
-              fillField(innerDocument, "Monday",    "UC_DAILYREST2$0", 'Y');
-              fillField(innerDocument, "Tuesday",   "UC_DAILYREST3$0", 'Y');
-              fillField(innerDocument, "Wednesday", "UC_DAILYREST4$0", 'Y');
-              fillField(innerDocument, "Thursday",  "UC_DAILYREST5$0", 'Y');
-              fillField(innerDocument, "Friday",    "UC_DAILYREST6$0", 'Y');
+              // Daily rest (Y,N or NA)
+              fillField(innerDocument, "Monday",    "select", "UC_DAILYREST2$0", 'Y', 'NA');
+              fillField(innerDocument, "Tuesday",   "select", "UC_DAILYREST3$0", 'Y', 'NA');
+              fillField(innerDocument, "Wednesday", "select", "UC_DAILYREST4$0", 'Y', 'NA');
+              fillField(innerDocument, "Thursday",  "select", "UC_DAILYREST5$0", 'Y', 'NA');
+              fillField(innerDocument, "Friday",    "select", "UC_DAILYREST6$0", 'Y', 'NA');
 
-              // Working start times
-              fillField(innerDocument, "Monday",    "UC_DAILYREST2$1", 'Y');
-              fillField(innerDocument, "Tuesday",   "UC_DAILYREST3$1", 'Y');
-              fillField(innerDocument, "Wednesday", "UC_DAILYREST4$1", 'Y');
-              fillField(innerDocument, "Thursday",  "UC_DAILYREST5$1", 'Y');
-              fillField(innerDocument, "Friday",    "UC_DAILYREST6$1", 'Y');
+              // Working start times (Y,N or NA)
+              fillField(innerDocument, "Monday",    "select", "UC_DAILYREST2$1", 'Y', 'NA');
+              fillField(innerDocument, "Tuesday",   "select", "UC_DAILYREST3$1", 'Y', 'NA');
+              fillField(innerDocument, "Wednesday", "select", "UC_DAILYREST4$1", 'Y', 'NA');
+              fillField(innerDocument, "Thursday",  "select", "UC_DAILYREST5$1", 'Y', 'NA');
+              fillField(innerDocument, "Friday",    "select", "UC_DAILYREST6$1", 'Y', 'NA');
 
-              // Working more than a half day
-              fillField(innerDocument, "Monday",    "UC_DAILYREST2$2", 'Y');
-              fillField(innerDocument, "Tuesday",   "UC_DAILYREST3$2", 'Y');
-              fillField(innerDocument, "Wednesday", "UC_DAILYREST4$2", 'Y');
-              fillField(innerDocument, "Thursday",  "UC_DAILYREST5$2", 'Y');
-              fillField(innerDocument, "Friday",    "UC_DAILYREST6$2", 'Y');
+              // Working more than a half day (Y,N or NA)
+              fillField(innerDocument, "Monday",    "select", "UC_DAILYREST2$2", 'Y', 'NA');
+              fillField(innerDocument, "Tuesday",   "select", "UC_DAILYREST3$2", 'Y', 'NA');
+              fillField(innerDocument, "Wednesday", "select", "UC_DAILYREST4$2", 'Y', 'NA');
+              fillField(innerDocument, "Thursday",  "select", "UC_DAILYREST5$2", 'Y', 'NA');
+              fillField(innerDocument, "Friday",    "select", "UC_DAILYREST6$2", 'Y', 'NA');
 
-              // Lunch time
-              fillField(innerDocument, "Monday",    "UC_TIME_LIN_WRK_UC_DAILYREST12$0", '1,0');
-              fillField(innerDocument, "Tuesday",   "UC_TIME_LIN_WRK_UC_DAILYREST13$0", '1,0');
-              fillField(innerDocument, "Wednesday", "UC_TIME_LIN_WRK_UC_DAILYREST14$0", '1,0');
-              fillField(innerDocument, "Thursday",  "UC_TIME_LIN_WRK_UC_DAILYREST15$0", '1,0');
-              fillField(innerDocument, "Friday",    "UC_TIME_LIN_WRK_UC_DAILYREST16$0", '1,0');
+              // Lunch time (in hours)
+              fillField(innerDocument, "Monday",    "input", "UC_TIME_LIN_WRK_UC_DAILYREST12$0", '1,0', '');
+              fillField(innerDocument, "Tuesday",   "input", "UC_TIME_LIN_WRK_UC_DAILYREST13$0", '1,0', '');
+              fillField(innerDocument, "Wednesday", "input", "UC_TIME_LIN_WRK_UC_DAILYREST14$0", '1,0', '');
+              fillField(innerDocument, "Thursday",  "input", "UC_TIME_LIN_WRK_UC_DAILYREST15$0", '1,0', '');
+              fillField(innerDocument, "Friday",    "input", "UC_TIME_LIN_WRK_UC_DAILYREST16$0", '1,0', '');
 
-              // Working location on mornings
-              fillField(innerDocument, "Monday",    "UC_LOCATION_A2$0", 'T');
-              fillField(innerDocument, "Tuesday",   "UC_LOCATION_A3$0", 'T');
-              fillField(innerDocument, "Wednesday", "UC_LOCATION_A4$0", 'C');
-              fillField(innerDocument, "Thursday",  "UC_LOCATION_A5$0", 'C');
-              fillField(innerDocument, "Friday",    "UC_LOCATION_A6$0", 'T');
+              // Working location on mornings (P,T,C,O,E,VCGI,VCLI or NA)
+              fillField(innerDocument, "Monday",    "select", "UC_LOCATION_A2$0", 'T', 'NA');
+              fillField(innerDocument, "Tuesday",   "select", "UC_LOCATION_A3$0", 'T', 'NA');
+              fillField(innerDocument, "Wednesday", "select", "UC_LOCATION_A4$0", 'C', 'NA');
+              fillField(innerDocument, "Thursday",  "select", "UC_LOCATION_A5$0", 'C', 'NA');
+              fillField(innerDocument, "Friday",    "select", "UC_LOCATION_A6$0", 'T', 'NA');
 
-              // Working location on afternoons
-              fillField(innerDocument, "Monday",    "UC_LOCATION_A2$1", 'T');
-              fillField(innerDocument, "Tuesday",   "UC_LOCATION_A3$1", 'T');
-              fillField(innerDocument, "Wednesday", "UC_LOCATION_A4$1", 'C');
-              fillField(innerDocument, "Thursday",  "UC_LOCATION_A5$1", 'C');
-              fillField(innerDocument, "Friday",    "UC_LOCATION_A6$1", 'T');
+              // Working location on afternoons (P,T,C,O,E,VCGI,VCLI or NA)
+              fillField(innerDocument, "Monday",    "select", "UC_LOCATION_A2$1", 'T', 'NA');
+              fillField(innerDocument, "Tuesday",   "select", "UC_LOCATION_A3$1", 'T', 'NA');
+              fillField(innerDocument, "Wednesday", "select", "UC_LOCATION_A4$1", 'C', 'NA');
+              fillField(innerDocument, "Thursday",  "select", "UC_LOCATION_A5$1", 'C', 'NA');
+              fillField(innerDocument, "Friday",    "select", "UC_LOCATION_A6$1", 'T', 'NA');
 
               let saveButton = innerDocument.getElementById("#ICSave");
               saveButton.addEventListener('click', function() {
